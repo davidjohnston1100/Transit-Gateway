e693f3e42a9734b937f0af68e06341eb957c7e93
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2022-12-22 21:10:54 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.0.0"
+    }
+  }
+}  
+
+provider "aws" {
+  alias  = "local"
+  region = "us-east-1"
+}
+
 module "tgw" {
-  source = "./modules/aws-s3-static-website-bucket"
+  source = "./modules/aws-transit-gateway"
+
+  provider = aws.local
+  description = var.description
   
   
+  /*tags = {
+    Name = "Local TGW"
+  }*/
   
-  
-  
-  
+  auto_accept_shared_attachments = var.auto_accept_shared_attachments
+  dns_support = var.dns_support
 }
