c5b227bc65e5383282a82f55415e23050de83438
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2022-12-22 21:58:51 -0600

    Update main.tf

--- modules/aws-transit-gateway/main.tf
+++ modules/aws-transit-gateway/main.tf
@@ -1,12 +1,3 @@
 resource "aws_ec2_transit_gateway" "this" {
-  #description = var.description
-  #provider = aws.local
-  
-  tags = {
-    Name = "Local TGW"
-  }
-  
-  #auto_accept_shared_attachments = var.auto_accept_shared_attachments
-  #dns_support = var.dns_support*/
 }
 
