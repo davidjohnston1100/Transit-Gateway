56ecacf67987ad9204afc55ba4cf71d58f0e06c2
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2022-12-23 23:08:43 -0600

    Update main.tf

--- modules/aws-transit-gateway/main.tf
+++ modules/aws-transit-gateway/main.tf
@@ -9,6 +9,8 @@ resource "aws_ec2_transit_gateway" "this" {
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
+  count = var.condition ? 1 : 0
+  
   subnet_ids         = var.subnet_ids
   transit_gateway_id = var.transit_gateway_id
   vpc_id             = var.vpc_id
