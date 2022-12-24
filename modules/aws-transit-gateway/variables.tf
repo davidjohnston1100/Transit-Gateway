84aee0294fdece3cda555aaddbb122688d816d8b
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2022-12-23 19:03:59 -0600

    Update variables.tf

--- modules/aws-transit-gateway/variables.tf
+++ modules/aws-transit-gateway/variables.tf
@@ -3,7 +3,14 @@ variable "description" {
   type        = string
   #default     = "example-vpc"
 }
+
 variable "auto_accept_shared_attachments" {
+  description = ""
+  type        = string
+  default     = "enable"
+}
+
+variable "dns_support" {
   description = ""
   type        = string
   default     = "enable"
