fa3f6fc260c4b7d04bf13d28c6fb42c5f105782b
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2022-12-23 19:14:59 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,11 @@ module "tgw" {
   description = "AWS-Transit-Gateway for US-East-1"
   auto_accept_shared_attachments = "enable"
   dns_support = "enable"
+  
   tags = {
     Name = "US-East-1 AWS-Transit-Gateway"
   }
+  
+  default_route_table_association = "enable"
+  default_route_table_propagation = "enable"
 }
