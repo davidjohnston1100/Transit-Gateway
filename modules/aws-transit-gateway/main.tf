286680a9daf90b95195d1103bcb3ddc280eb5bba
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2022-12-23 19:03:22 -0600

    Update main.tf

--- modules/aws-transit-gateway/main.tf
+++ modules/aws-transit-gateway/main.tf
@@ -1,5 +1,6 @@
 resource "aws_ec2_transit_gateway" "this" {
   description = var.description
   auto_accept_shared_attachments = var.auto_accept_shared_attachments
+  dns_support = var.dns_support
 }
 
