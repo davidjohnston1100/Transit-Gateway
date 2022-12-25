d1ee1db45b7454f3759701adf43fe187e671f58f
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2022-12-24 20:12:59 -0600

    Update outputs.tf

--- modules/aws-transit-gateway/outputs.tf
+++ modules/aws-transit-gateway/outputs.tf
@@ -3,6 +3,11 @@ output "tgw_id" {
   value       = aws_ec2_transit_gateway.this.id
 }
 
+output "tgw_default_route_table_id" {
+  description = "EC2 Transit Gateway default route table identifier"
+  value       = aws_ec2_transit_gateway.this.association_default_route_table_id
+}
+
 output "tgw_attachment_id" {
   description = "EC2 Transit Gateway attachment identifier"
   value       = aws_ec2_transit_gateway_vpc_attachment.this.id
