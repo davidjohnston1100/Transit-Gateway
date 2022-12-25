40c82f39285a6cec96ae5d7703671576015413d9
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2022-12-24 20:05:00 -0600

    Update outputs.tf

--- modules/aws-transit-gateway/outputs.tf
+++ modules/aws-transit-gateway/outputs.tf
@@ -2,3 +2,8 @@ output "tgw_id" {
   description = "EC2 Transit Gateway identifier"
   value       = aws_ec2_transit_gateway.this.id
 }
+
+output "tgw_attachment_id" {
+  description = "EC2 Transit Gateway attachment identifier"
+  value       = aws_ec2_transit_gateway_vpc_attachment.this.id
+}
