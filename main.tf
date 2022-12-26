baa4e99c8af8b6191bb510fe491624f98a82d3e2
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2022-12-26 12:28:25 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,14 +26,17 @@ module "tgw" {
   source = "./modules/aws-transit-gateway"
   
   description = var.description 
-  tags = var.tgw_name
+  
+  tags = {
+    Name = "US-East-1 AWS-Transit-Gateway"
+  }
   
   subnet_ids         = module.vpc.private_subnets
   transit_gateway_id = module.tgw.tgw_id
   vpc_id             = module.vpc.vpc_id
     
-  peer_region             = var.peer_region
-  peer_transit_gateway_id = var.peer_transit_gateway_id
+  #peer_region             = var.peer_region
+  #peer_transit_gateway_id = var.peer_transit_gateway_id
     
   destination_cidr_block_1 = "10.6.2.0/24"
   transit_gateway_attachment_id  = module.tgw.tgw_attachment_id
