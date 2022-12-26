6368b94770a07c0259daef20e6ca7a74e41e241c
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2022-12-26 14:28:02 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,14 +35,15 @@ module "tgw" {
   transit_gateway_id = module.tgw.tgw_id
   vpc_id             = module.vpc.vpc_id
     
-  #peer_region             = var.peer_region
-  #peer_transit_gateway_id = var.peer_transit_gateway_id
+  create_peer_attachment  = var.reate_peer_attachment
+  peer_region             = var.peer_region
+  peer_transit_gateway_id = var.peer_transit_gateway_id
     
   destination_cidr_block_1 = "10.6.2.0/24"
   transit_gateway_attachment_id  = module.tgw.tgw_attachment_id
   transit_gateway_route_table_id = module.tgw.tgw_default_route_table_id
     
   destination_cidr_block_2 = "10.6.3.0/24"
-  destination_cidr_block_3 = "10.2.0.0/16"
-  destination_cidr_block_4 = "10.5.0.0/16"
+  #destination_cidr_block_3 = "10.2.0.0/16"
+  #destination_cidr_block_4 = "10.5.0.0/16"
 }
